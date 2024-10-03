b9f6c53132b02d1732acf76f1361722b627af54e
Rian Oktio Mersa Putra <fueremimp@gmail.com>
2024-10-03 09:32:11 +0700

    fix: name guest on whatsapp always capitilize when using all uppercase

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
 Assalamualaikum Wr. Wb.
 
 Yth. ${name.replace(/\w\S*/g, function (txt) {
-      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+      return txt.charAt(0).toUpperCase() + txt.substr(1);
     })}
 
 Tanpa mengurangi rasa hormat, perkenankan kami mengundang Bapak/Ibu/Saudara/i, teman sekaligus sahabat, untuk menghadiri acara pernikahan kami :
